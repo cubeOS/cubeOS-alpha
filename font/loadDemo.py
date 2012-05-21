ed95cbbc3b4df4130b5bc8bdbedc196833bada1d
Sasha Crofter <sasha@crofter.org>
2012-05-20 23:47:57 -0400

    added new demo and scripts for generating it

---
+++ font/loadDemo.py
@@ -0,0 +1,22 @@
+output = "SET A, 0\n\
+SET B, videoram \n\
+HWI 0 \n\
+\n\
+SET A, 1 \n\
+SET B, fontram \n\
+HWI 0 \n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; Designated Video RAM ;\n\
+;;;;;;;;;;;;;;;;;;;;;;;;\n\
+\n\
+:fontRam\n\
+DAT "
+
+output += open("values.txt", "r").read()
+
+output += "\n\n:videoram\nDAT "
+
+output += open("fontChars.txt", "r").read()
+
+print(output)
--- font/printFontChars.py
+++ font/printFontChars.py
@@ -6,7 +6,7 @@ while(i<128):
 	if(len(x) < 2):
 		x = "0" + x
 
-	out = out + "0xF0" + x + ", "
+	out += "0xF0" + x + ", "
 	i += 1
 
 out = out[:-2]
