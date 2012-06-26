618d3df62c6af3e3a4ab30caa67b869c338917ee
Sasha Crofter <sasha@LOVELACE>
2012-06-26 19:24:52 -0400

    worked on new script

--- update.py
+++ update.py
@@ -1,6 +1,21 @@
-LOCATION=""
+import sys, os, glob
 
-PATHTOASSEMBLER="das"
-PATHTOEMULATOR="dcpu-16"
+kernel = ""
+location = os.path.dirname(os.path.abspath(sys.argv[0])) + os.sep
+modLoc = location + "modules/"
 
-for i in 
+modules = []
+moduleExtention = ".cubeos"
+
+packageModule = ""
+
+modules.append(modLoc + "environment" + moduleExtention)
+modules.append(modLoc + "boot" + moduleExtention)
+
+for files in glob.glob(location + "modules/*" + moduleExtention):
+	if files != modules[0] and files != modules[1]:
+		modules.append(files)
+
+
+kernel += open(location + "LICENSE", 'r').read()
+print(modules)
