0254af65fd6d8ab37fc800f73adde120cf0cfa7a
Sasha Crofter <sasha@crofter.org>
2012-06-26 20:52:48 -0400

    NEW VERSION 0.5
    made package addition much, much easier
    adjusted package.cubeos to comply
    
    finished build.py script
    moved collect to collect.depricated
    removed unnecessary packages

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys, os, glob
 
 kernel = ""
@@ -11,8 +13,9 @@ modules = []
 packLoc = location + "modules/packages/"
 packageExtention = ".package"
 
-packageModule = ""
+packageModule = modLoc + "package" + moduleExtention
 builtPackageModule = ""
+packageLoads = ""
 packages = ""
 
 
@@ -21,18 +24,28 @@ modules.append(modLoc + "environment" + moduleExtention)
 modules.append(modLoc + "boot" + moduleExtention)
 
 for files in glob.glob(modLoc + "*" + moduleExtention):
-	if files != modules[0] and files != modules[1]:
+	if files != modules[0] and files != modules[1] and files != packageModule:
 		modules.append(files)
 
 # Create a string containing all packages
+# also create a DASM string to load the packages
 for files in glob.glob(packLoc + "*" + packageExtention):
     packages += open(files, 'r').read()
+    filename = os.path.splitext(os.path.basename(files))[0]
+    packageLoads += "\nSET A, package." + filename + "\nJSR p.loadPackage\n"
 
-builtPackageModule = 
+builtPackageModule = open(packageModule, 'r').read().replace(";>loadcalls", packageLoads, 1).replace(";>packages", packages, 1)
 
 kernel += open(location + "LICENSE", 'r').read()
 
 for i in range(0, len(modules)):
     kernel += open(modules[i], 'r').read() + "\n; ; ; ;\n"
 
+kernel += builtPackageModule
 
+# Overwrite old kernel
+kernelf = open(location + "cubeOS.dasm16", 'w')
+kernelf.seek(0)
+kernelf.write(kernel)
+kernelf.truncate()
+kernelf.close()
