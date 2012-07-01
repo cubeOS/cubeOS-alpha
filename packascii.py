8db1bd4a159fa5f7c06688ad4248780ee676b5b0
Dylan Whichard <dylan@whichard.com>
2012-06-30 23:19:45 -0400

    Fixed some minor issues in packascii.py

--- packascii.py
+++ packascii.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python
 import sys
-str = raw_input(">")
 shift = 1;
 chars = []
-for c in str:
-	n = ord(c)
+print 'Enter an empty line to exit'
+while 1:
+	str = raw_input(">")
+	if str=='': break
+	for c in str:
+		n = ord(c)
+		if shift:
+			chars.append(n << 8)
+			shift = 0
+		else:
+			shift = 1
+			chars[-1] |= n
+	#add a newline where the enter key was pressed
 	if shift:
-		chars.append(n << 8)
+		chars.append(0x0A << 8)
 		shift = 0
 	else:
 		shift = 1
-		chars[-1] |= n
+		chars[-1] |= 0x0A
 sys.stdout.write('DAT ')
+#but remove the very last newline because it actually shouldn't be there
+if shift: chars[-1] &= 0xFF00
+else: chars[-1] = 0x0000
 last = ''
 for n in chars:
 	if (last !=''): sys.stdout.write(last+', ')
 	last = hex(n)
-print(last)
+print last
