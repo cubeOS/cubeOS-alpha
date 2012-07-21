85307a1ddddd249ebea46c609e5e00fe518055a3
Dylan Whichard <dylan@whichard.com>
2012-07-21 01:56:13 -0400

    Actually tested/fixed run.py on Windows and removed old run script

--- run.py
+++ run.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
-# This is a script to automate building, compiling, and assembling on 
-# Linux. A platform-independent version will be made available soon. You 
-# may have to change the line `dcpu cubeOS.bin` to `dcpu-16 cubeOS.bin`, 
-# depending on your install of Benedek's emulator.
-import os
-
+# This is a platform-independent script to automate building, compiling,
+# and assembling cubeOS. Windows users must have das and dcpu-16 binaries
+# in the cubeOS directory, or in their system PATH location
+from subprocess import call
 import build	#Runs build.py
-assembleStatus = os.spawnlp(os.P_WAIT,"das","das","-o","cubeOS.bin","cubeOS.dasm16")
-if (assembleStatus==os.EX_OK):
-	runStatus = os.spawnlp(os.P_WAIT,"dcpu-16","dcpu-16","cubeOS.bin")
-	if (runStatus!=os.EX_OK): print("Error, dcpu-16 returned exit code %d".format(runStatus))
+assembleStatus = call(["das","-o","cubeOS.bin","cubeOS.dasm16"])
+if (assembleStatus==0):
+	runStatus = call(["dcpu-16","cubeOS.bin"])
+	print "dcpu-16 execution finished with status",runStatus
 else:
-	print("Error, das returned exit code %d".format(assembleStatus))
-
+	print "**ASSEMBLY FAILED WITH STATUS ",assembleStatus,"**"
