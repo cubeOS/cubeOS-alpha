3330151d681e0c0449fe46a9acc6e78af93385e0
Braden Shepherdson <braden.shepherdson@gmail.com>
2012-07-24 18:53:51 -0400

    More bugfixes and debugging.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from subprocess import call
 import build	#Runs build.py
 assembleStatus = call(["das","-o","cubeOS.bin","cubeOS.dasm16"])
 if (assembleStatus==0):
-	runStatus = call(["dcpu-16","cubeOS.bin"])
+	runStatus = call(["dcpu-16","-s","cubeOS.cefs","cubeOS.bin"])
 	print "dcpu-16 execution finished with status",runStatus
 else:
 	print "**ASSEMBLY FAILED WITH STATUS ",assembleStatus,"**"
